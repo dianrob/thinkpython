01fc04fb06df0a4f58ce5b4b6d3101e12f58554c
dianrob <robert.synofzik@gmail.com>
2019-05-31 12:53:57 +0200

    fiddle diddle

--- moby.py
+++ moby.py
@@ -1,25 +1,63 @@
-moby = open('words.txt')
+fin = open('words.txt')
 
 
-def readline(words):
-    m = []
-    for line in words:
-        y = words.readline()
-        m.append(y.strip())
-    return m
+def fin_strip(m):
+    x = []
+    for line in m:
+        word = line.strip()
+        x.append(word)
+    return x
 
 
-def bigger20(m):
-    print(' '.join([i for i in m if len(i) > 19]))
+def over20(m):
+    y = fin_strip(m)
+    for i in y:
+        if len(i) > 19:
+            print(i)
 
 
 def has_no_e(word):
-    if 'e' not in word:
-        print('True')
+    y = fin_strip(word)
+    z = len(y)
+    count = 0
+    for i in y:
+        if 'e' not in i:
+            print(i)
+            count += 1
+    print(count * 100 / z)
+
+# exercise 9.3
 
 
-has_no_e('banana')
+def avoid(word, string):
+    if string not in word:
+        return True
 
 
+# exercise 9.4
 
-#bigger20(readline(moby))
+def uses_only(word, string):
+    for letter in word:
+        if letter not in string:
+            return False
+    return True
+
+# exercise 9.5
+
+
+def uses_all(word, string):
+    for letter in string:
+        if letter not in word:
+            return False
+    return True
+
+
+def list_check(f):
+    x = []
+    for i in f:
+        if uses_all(i, 'aeiouy'):
+            x.append(i)
+    print(len(x))
+
+
+list_check(fin)
